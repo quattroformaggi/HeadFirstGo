323bc44873bcaeba31e9a3b770ed8bff8a281825
Alexa <64697981+quattroformaggi@users.noreply.github.com>
2021-11-11 00:40:08 +0300

    Add files via upload

---
+++ tok1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace _1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Разработайте программу, в случайных местах на
+             * консоли с частотой в 250 мс выводящую строку «C#». сдано2710*/
+            ConsoleKeyInfo info;
+            Random random = new Random();
+            while (true)
+            {
+                Console.SetCursorPosition(random.Next(1, Console.WindowHeight), random.Next(1, Console.WindowWidth));
+                Console.WriteLine("C#");
+                if (Console.KeyAvailable)
+                {
+                    info = Console.ReadKey(true);
+                    if (info.Key == ConsoleKey.E)
+                        break;
+                }
+                Thread.Sleep(250);
+            }
+            Console.Write("Нажмите любую клавишу, чтобы завершить...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ tok2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_8
+{
+    class Program
+    {
+        /* Напишите метод, принимающий параметром массив чисел,
+        и который будет возвращать минимальное значение,
+        максимальное значение и среднее значение среди положительных
+        элементов массива. Можете попробовать сделать это двумя
+        способами: через out параметры и при помощи кортежа. сдано2710 */
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int[] testarr = new int[10];
+            for (int i = 0; i < testarr.Length; i++)
+            {
+                testarr[i] = r.Next(-10000, 10000);
+                Console.Write(testarr[i] + "\t");
+            }
+            MinMaxAvg(testarr);
+        }
+        public static void MinMaxAvg(int[] array)
+        {
+            int sum = 0;
+            int down_to_min = int.MaxValue;
+            int up_to_max = int.MinValue;
+            foreach(int num in array)
+            {
+                if(num > -1)
+                {
+                    if (num < down_to_min)
+                        down_to_min = num;
+                    else if (num > up_to_max)
+                        up_to_max = num;
+                    sum += num;
+                }
+            }
+            Console.WriteLine("\n\nСумма:"+ sum+ "\nМин:"+ down_to_min+ "\nМакс:"+ up_to_max);
+            Console.ReadKey();
+        }
+    }
+}
